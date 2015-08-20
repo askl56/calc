1ca5fb98a031999a477206cac4f6f0270e0d9de6
Andrew Scott <amscott@openmailbox.org>
2015-08-20 10:00:01 +0100

    added tests

---
+++ test/math_expression_test.rb
@@ -0,0 +1,17 @@
+class TestMathExpression < Minitest::Test
+  def test_number
+    assert_equal 9, MathExpression.calculate("9")
+  end
+
+  def test_simple_addition
+    assert_equal 5, MathExpression.calculate("2 PLUS 3")
+  end
+
+  def test_simple_subtraction
+    assert_equal 3, MathExpression.calculate("5 MINUS 2")
+  end
+
+  def test_complex_calculation
+    assert_equal 14, MathExpression.calculate("4 PLUS 7 MINUS 1 PLUS 4")
+  end
+end
