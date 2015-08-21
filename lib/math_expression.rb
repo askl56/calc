dc8f2eedcf2252ed55ee12c366e3bf5756bbbeaf
Andrew Scott <amscott@openmailbox.org>
2015-08-21 11:42:55 +0100

    finished calculate function. Now time to refactor

--- lib/math_expression.rb
+++ lib/math_expression.rb
@@ -1,37 +1,26 @@
 class MathExpression
 
   def self.calculate(string)
-    # matches = /(\d+)\s*(\w+)\s?(\d+)/.match(string)
-    # num1 = matches[1].to_i
-    # num2 = matches[3].to_i
-    # action = matches[2]
-
-    # if matches.size == 1
-    #   return num1
-    # elsif action == "PLUS"
-    #   return (num1 + num2)
-    # elsif action == "MINUS"
-    #   return (num1 - num2)
-    # else
-    #   return num1
-    # end
-
     value = string.split(" ")
-    shortened_array = value.map{ |v| parser(v) }.compact!
+    shortened_array = value.map{ |v| parser(v) }.compact
     head, *tail = shortened_array
-    tail.each_slice(2).inject(head) { |val1, next| val1.send(*next_op)}
-
+    tail.each_slice(2).inject(head) { |b1, operation| b1.send(*operation)}
   end
+
+  OPERATOR = {
+    "PLUS" => "+",
+    "MINUS" => "-"
+  }
 
   private
 
-  def is_number?(value)
-    value.to_i == value
+  def self.is_number?(value)
+    Integer(value) rescue false
   end
 
-  def parser(value)
+  def self.parser(value)
     if OPERATOR.keys.include? value
-      return OPERATOR[key]
+      return OPERATOR[value]
     end
 
     if is_number?(value)
