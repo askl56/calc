e46f68d48bc54e43498042d0b9546dddbec7150d
askl56 <amscott@openmailbox.org>
2015-08-27 13:41:44 +0100

    added formatter for cli

--- lib/expression_formatter.rb
+++ lib/expression_formatter.rb
@@ -1,3 +1,14 @@
 class ExpressionFormatter
 
+  def initialize(result)
+    @result = result
+  end
+
+  # def default_format(expression, answer)
+  #   return Hash.new {"expression" => expression, "answer" => answer}
+  # end
+
+  def executable_format(result)
+    puts "The answer to your query is #{result}"
+  end
 end
